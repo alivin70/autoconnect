02da68c793a1718f1066accf53df4628a0f5cd07
niko95j <nicolagreco92@gmail.com>
2019-05-15 16:50:39 +0200

    Fix DHCPAttempt

--- autoconnect/DHCPAttempt.py
+++ autoconnect/DHCPAttempt.py
@@ -10,13 +10,14 @@ class DHCPAttempt(ConnectionAttempt):
     def connect(self):
         localiface = 'ens3'
         localmac = get_if_hwaddr(localiface)
-        print(localmac)
+        fam, raw = get_if_raw_hwaddr(localiface)
+        print(raw)
 
         # make DHCP DISCOVER
         dhcp_discover = Ether(src=localmac, dst='ff:ff:ff:ff:ff:ff') / IP(src='0.0.0.0', dst='255.255.255.255') / UDP(
-            dport=67, sport=68) / BOOTP(chaddr=localmac, xid=RandInt()) / DHCP(
+            dport=67, sport=68) / BOOTP(chaddr=raw, xid=RandInt()) / DHCP(
             options=[('message-type', 'discover'), 'end'])
         print(dhcp_discover.display())
-        dhcp_offer = srp1(dhcp_discover, iface=localiface)
+        #dhcp_offer = srp1(dhcp_discover, iface=localiface)
 
         #print(dhcp_offer.display())
