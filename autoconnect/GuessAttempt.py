95cf8bf3e6e8cf1508792c55c2366aedddec23e4
niko95j <nicolagreco92@gmail.com>
2019-05-25 19:53:04 +0200

    Added free ip address finder in GuessAttempt

--- autoconnect/GuessAttempt.py
+++ autoconnect/GuessAttempt.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 from ConnectionAttempt import ConnectionAttempt
 from ARPTable import *
-from Util import *
+from ipaddress import *
 
 
 class GuessAttempt(ConnectionAttempt):
@@ -11,8 +11,8 @@ class GuessAttempt(ConnectionAttempt):
         self.arp_table = ARPTable()
         self.acc_and = 0xffffffff
         self.acc_or = 0x00000000
-        self.net_address = 0x00000000
-        self.subnet_mask = 0x00000000
+        self.network = None
+        self.gateway = None
         self.ignore_ip = {'0.0.0.0', '192.168.1.1', '192.168.1.49', '160.97.146.123', '160.97.144.1', '160.97.147.90',
                          '160.97.144.3', '160.97.146.99', '160.97.145.57', '160.97.145.18', '160.97.146.217',
                          '160.97.147.0', '160.97.146.19', '160.97.144.2', '160.97.146.1', '160.97.145.172',
@@ -23,28 +23,69 @@ class GuessAttempt(ConnectionAttempt):
         # for entry in self.arp_table.table:
         #     print(self.arp_table.table[entry].ip_address + "\t" + self.arp_table.table[entry].mac_address + "\t" +
         #           str(self.arp_table.table[entry].count))
-        self.infer_network()
+        self.network_discover()
+        self.find_gateway()
+        print("Network: " + str(self.network))
+        print("Default gateway: " + str(self.gateway))
+        ip = self.find_ip()
+        print("IP address: " + str(ip))
 
-    def infer_network(self):
+    def network_discover(self):
         # TODO Fix subnet_mask in case of ones after the first zero.
-        self.net_address = self.acc_and & self.acc_or
-        print(int_to_ip(self.net_address))
-        self.subnet_mask = self.acc_and ^ self.acc_or
-        self.subnet_mask = 0xffffffff - self.subnet_mask
-        print(int_to_ip(self.subnet_mask))
+        net_address = self.acc_and & self.acc_or
+        subnet_mask = self.acc_and ^ self.acc_or
+        subnet_mask = 0xffffffff - subnet_mask
 
+        net_address_str = str(IPv4Address(net_address))
+        net_address_str += "/" + str(IPv4Address(subnet_mask))
+
+        self.network = IPv4Network(net_address_str)
+
+    def find_gateway(self):
+        max_count = 0
+        gateway = None
+        for entry in self.arp_table.table:
+            if self.arp_table.table[entry].count > max_count:
+                max_count = self.arp_table.table[entry].count
+                gateway = self.arp_table.table[entry].ip_address
+        self.gateway = ip_address(gateway)
+
+    def find_ip(self):
+        free_ip = None
+        hosts = list(self.network.hosts())
+        tmp_ip = hosts[random.randint(0, len(hosts))]
+        print(tmp_ip)
+        for ip in hosts:
+            if not self.arp_table.contains(str(ip)):
+                ip_dst = str(ip)
+                print(ip_dst)
+                arp_request = self.make_arp_request(tmp_ip, ip_dst)
+                arp_reply = srp1(arp_request, timeout=3, verbose=0)
+                if arp_reply is not None:
+                    print(arp_reply.display())
+                else:
+                    free_ip = ip
+                    break
+        return free_ip
+
+    def make_arp_request(self, ip_src, ip_dst):
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff", src=self.mac_address)
+        arp = ARP(op=1, hwsrc=self.mac_address, psrc=ip_src,
+                  hwdst="ff:ff:ff:ff:ff:ff", pdst=ip_dst)
+        pkt = ether / arp
+        return pkt
 
     def sniff(self):
         packets = sniff(filter="arp", prn = self.arp_process,
                         offline="/home/nigre/Documents/Thesis/wiresharkcap-root.pcapng")
 
-    def add_ip(self, ip_address):
-        ip = ip_to_int(ip_address)
+    def add_ip(self, ip_addr):
+        ip = int(IPv4Address(ip_addr))
         self.acc_and &= ip
         self.acc_or |= ip
 
     def arp_process(self, pkt):
-        # TODO check ip with arp-request ???
+        # TODO check ip with arp-request (to avoid processing IP of a different subnet)
         ip_src = pkt[ARP].psrc
         mac_src = pkt[ARP].hwsrc
         if ip_src not in self.ignore_ip:
