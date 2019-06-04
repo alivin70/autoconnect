aca66cc9175690867e12d95341cf2d5bb7de36be
niko95j <nicolagreco92@gmail.com>
2019-06-04 18:41:22 +0200

    Added credentials to captive portal

--- autoconnect/CaptivePortalHandler.py
+++ autoconnect/CaptivePortalHandler.py
@@ -18,44 +18,47 @@ class CaptivePortalHandler:
 
         url = resp.url.split("?", 1)[0]
 
+        f = open("resources/credentials")
+
         for domain in self.domains:
-            username = "admin"
-            password = "zeroshell"
-            realm = domain
-            zscp_redirect = "_:::_"
-            renew_interval = 40
-            print(username, password, realm)
+            for line in f:
+                credentials = line.strip().split(",")
+                username = credentials[0]
+                password = credentials[1]
+                realm = domain
+                zscp_redirect = "_:::_"
+                print(username, password, realm)
 
-            params = {self.username_field_name: username, self.password_field_name: password, self.domain_name: realm, 'Section': 'CPAuth',
-                      'Action': 'Authenticate', 'ZSCPRedirect': zscp_redirect}
-            resp = get(url, params=params)
-            html = resp.text
+                params = {self.username_field_name: username, self.password_field_name: password, self.domain_name: realm, 'Section': 'CPAuth',
+                          'Action': 'Authenticate', 'ZSCPRedirect': zscp_redirect}
+                resp = get(url, params=params)
+                html = resp.text
 
-            if 'Access Denied' in html:
-                print("Wrong username or password")
+                if 'Access Denied' in html:
+                    print("Wrong username or password")
 
-            else:
-                authkey = self.find_authkey(html)
-                if authkey is not None:
-                    params = {self.username_field_name: username, self.password_field_name: password, self.domain_name: realm,
-                              'Authenticator': authkey, 'Section': 'CPGW', 'Action': 'Connect', 'ZSCPRedirect': zscp_redirect}
-                    resp = get(url, params=params)
-                    print(resp.status_code)
-
-                    params = {self.username_field_name: username, self.password_field_name: password, self.domain_name: realm,
-                              'Authenticator': authkey, 'Section': 'ClientCTRL', 'Action': 'Connect',
-                              'ZSCPRedirect': zscp_redirect}
-                    resp = get(url, params=params)
-                    print(resp.status_code)
-
-                    resp = request(method='GET', url="http://clients3.google.com/generate_204", allow_redirects=False)
-                    if resp.status_code == 204:
-                        print("Successfully connected!")
-                        return
-                    else:
-                        print("Unable to connect!")
                 else:
-                    print("No authentication key")
+                    authkey = self.find_authkey(html)
+                    if authkey is not None:
+                        params = {self.username_field_name: username, self.password_field_name: password, self.domain_name: realm,
+                                  'Authenticator': authkey, 'Section': 'CPGW', 'Action': 'Connect', 'ZSCPRedirect': zscp_redirect}
+                        resp = get(url, params=params)
+                        print(resp.status_code)
+
+                        params = {self.username_field_name: username, self.password_field_name: password, self.domain_name: realm,
+                                  'Authenticator': authkey, 'Section': 'ClientCTRL', 'Action': 'Connect',
+                                  'ZSCPRedirect': zscp_redirect}
+                        resp = get(url, params=params)
+                        print(resp.status_code)
+
+                        resp = request(method='GET', url="http://clients3.google.com/generate_204", allow_redirects=False)
+                        if resp.status_code == 204:
+                            print("Successfully connected!")
+                            return
+                        else:
+                            print("Unable to connect!")
+                    else:
+                        print("No authentication key")
 
     def find_input_fields(self, resp):
         parser = AdvancedHTMLParser()
